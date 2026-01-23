3dfc9969a9b47a048590cfdba8743009b9b09e60
Atharv Tijare <anshutijare05@gmail.com>
2026-01-23 19:17:34 +0530

    add

--- firstclass.java
+++ firstclass.java
@@ -3,3 +3,9 @@ class firstclass {
         System.out.println("Hello World");
     }
 }
+
+class secondclass {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Java");
+    }
+}
