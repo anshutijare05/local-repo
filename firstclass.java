55b96f37a9a88524252f9e330ed30c2e84189369
Atharv Tijare <anshutijare05@gmail.com>
2026-01-23 19:31:18 +0530

    add my java code

--- firstclass.java
+++ firstclass.java
@@ -9,3 +9,22 @@ class secondclass {
         System.out.println("Welcome to Java");
     }
 }
+
+class thirdclass {
+    public static void main(String args[]) {
+        //output Hello World
+        System.out.println("Hello World");
+        System.out.print("Hello World");
+    }  
+}
+// 1st line - we've used println function instead of print function to print the text in a new line after execution
+// 2nd line - if we'd used print function it would have printed the next output in the same line
+
+class fourthclass {
+    public static void main(String args[]) {
+        //output Welcome to Java
+        System.out.print("Welcome to Java\n");
+        System.out.print("Welcome to Java");
+        //we've used \n escape sequence to print the next output in a new line
+    }  
+}
