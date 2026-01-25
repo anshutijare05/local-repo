5aee6e38a0b22f53faf3a5b7dbf86ca7b7cd3ce6
Atharv Tijare <anshutijare05@gmail.com>
2026-01-25 14:52:22 +0530

    add new changes to java files

--- firstclass.java
+++ firstclass.java
@@ -1,21 +1,24 @@
-class firstclass {
+public class firstclass {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        System.out.println("Hello Folks");
+        secondclass.main(args);
     }
 }
-
+    
 class secondclass {
     public static void main(String args[]) {
         System.out.println("Welcome to Java");
+        thirdclass.main(args);
     }
 }
 
 class thirdclass {
     public static void main(String args[]) {
         //output Hello World
-        System.out.println("Hello World");
-        System.out.print("Hello World");
-    }  
+        System.out.println("I am Atharv");
+        System.out.print("A 20 y/o coder\n");
+        fourthclass.main(args);
+    }
 }
 // 1st line - we've used println function instead of print function to print the text in a new line after execution
 // 2nd line - if we'd used print function it would have printed the next output in the same line
@@ -23,8 +26,29 @@ class thirdclass {
 class fourthclass {
     public static void main(String args[]) {
         //output Welcome to Java
-        System.out.print("Welcome to Java\n");
-        System.out.print("Welcome to Java");
+        System.out.print("Learning Java\n");
+        System.out.print("And exploring it\n");
         //we've used \n escape sequence to print the next output in a new line
-    }  
+        fifthclass.main(args);
+    }
+}
+class fifthclass {
+    public static void main(String args[]) {
+        //output Hello World Welcome to Java
+        System.out.print("And later onn\nI am going to master it\n\n\n");
+        //we've used \n escape sequence to print the next output in a new line
+        // we can give multiple \n to give multiple lines of space
+        sixthclass.main(args);
+    }
+}
+
+class sixthclass {
+    public static void main(String args[]) {
+        //"*" question, my first java problem of 2026
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
 }
